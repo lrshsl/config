db4c1015c95def8df4e481391b8f55172a6cfdf6
lrshsl <hoeslilars@gmail.com>
2023-07-11 06:26:03 -0400

    add statup script

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+setxkbmap "us(colemak),ch" -option "grp:shifts_toggle"
+
+nitrogen --restore
+# urxvtd &
+# nm-applet
+# volumeicon
+# conky -c $HOME/config/div/conky_config
+# pamac-tray
+# xfce4-power-manager
+# blueberry-tray
+# numlockx on
+
