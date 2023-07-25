60c369d751bc590c76017ea35953bdd3b741d2df
Lars <96301469+lrshsl@users.noreply.github.com>
2023-07-25 17:01:07 -0400

    Update aliases.fish

--- aliases.fish
+++ aliases.fish
@@ -2,23 +2,27 @@ function clear_screen
 	clear && echo
 end
 
-abbr c		clear_screen
-abbr q		exit
-abbr rbt	reboot
-abbr po		poweroff
-abbr get	'sudo pacman -S'
-abbr upd	'sudo pacman -Syyu --noconfirm'
-abbr rem	'sudo pacman -Rns'
+abbr c			clear_screen
+abbr q			exit
+abbr rbt		reboot
+abbr po			poweroff
+abbr get		'sudo pacman -S'
+abbr upd		'sudo pacman -Syyu --noconfirm'
+abbr rem		'sudo pacman -Rns'
+abbr fn			'funced -e nvim -s'
 
-abbr find	fd
-abbr grep	rg
+abbr find		fd
+abbr grep		rg
 
-alias l		'exa'
-alias ls	'exa -a'
-alias la	'exa -la'
-alias lst	'exa -aT'
-alias lst2	'exa -aT -L 2'
-alias lst3	'exa -aT -L 3'
-alias lst4	'exa -aT -L 4'
-alias lst5	'exa -aT -L 5'
+alias t			'exa -TL 1'
+
+alias l			'exa -l'
+alias ll		'exa -lah'
+alias ls		'exa -la'
+alias la		'exa -la'
+alias lst		'exa -aT'
+alias l2		'exa -aT -L 2'
+alias l3		'exa -aT -L 3'
+alias l4		'exa -aT -L 4'
+alias l5		'exa -aT -L 5'
 
